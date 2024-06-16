561be6a101ed5193541de3c8c30c3b38bf0ad5c3
inomoto <medalhkr@gmail.com>
2024-06-16 23:40:17 +0900

    parseしてみる
    ライブラリは例によってざっと調べた感じ。ただlow-levelなものは避けた

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let body = reqwest::blocking::get("https://www.example.com")?.text()?;
-
     println!("{body}");
+    println!("=====");
+
+    let document = scraper::Html::parse_document(&body);
+    let selector = scraper::Selector::parse("div > p > a").unwrap();
+    let elements = document.select(&selector);
+
+    elements.for_each(|e| println!("{}", e.text().next().unwrap()));
+
     return Ok(());
 }
